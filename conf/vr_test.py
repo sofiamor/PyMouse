ecfbcc2624749557877e0e7f3d8fa4c2a4400e1b
sofiamor <med2p1080156@med.uoc.gr>
2021-05-13 00:14:09 +0300

    some changes in VR

--- conf/vr_test.py
+++ conf/vr_test.py
@@ -1,11 +1,14 @@
 # Object experiment
+from typing import Any, Callable
+
 import numpy as np
 from Experiments.VR import *
 from Behavior import *
 from Stimuli.VROdors import *
 from utils.Generator import *
 
-
+interp = lambda x: interpolate.splev(np.linspace(0, len(x), 100),
+                                     interpolate.splrep(np.linspace(0, len(x), len(x)), x)) if len(x) > 3 else x
 conditions = []
 
 # define session parameters
@@ -59,7 +62,7 @@ resp_loc_y = 7
 
 # correct_loc = [(-0.25,0),(0.25,0),(0.25,0),(-0.25,0)]
 
-rand_theta = lambda: np.interp(np.random.randint(6)* np.pi / 180)
+rand_theta = lambda: interp(np.random.randint(6)* np.pi / 180)
 conditions += factorize({**key,
                          'difficulty'          : 2,
                          'odor_id'             : [[3, 4, 1, 2]],
@@ -75,8 +78,8 @@ conditions += factorize({**key,
                          'small_radius'        : 0.05,
                          'response_duration'   : 240000})
 
-rand_theta = lambda: np.interp(np.random.randint(6)* np.pi / 180)
-init_position = lambda: np.interp(np.random.randint(10, size = (1)))
+rand_theta = lambda: interp(np.random.randint(6)* np.pi / 180)
+init_position = lambda: interp(np.random.randint(10, size = (1)))
 conditions += factorize({**key,
                          'difficulty': 3,
                          'odor_id': [[1, 2, 3, 4]],
@@ -94,7 +97,7 @@ conditions += factorize({**key,
 
 # run experiments
 exp = State()
-exp.setup(Logger, VRBehavior, VROdors, session_params, conditions)
+exp.setup(logger, VRBehavior, VROdors, session_params, conditions)
 exp.run()
 
 
