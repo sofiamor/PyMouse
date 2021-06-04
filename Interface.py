0ab548ec0cefddf07a7b68a69875cdd06448b447
sofiamor <med2p1080156@med.uoc.gr>
2021-06-04 14:32:25 +0300

    VR changes :)

--- Interface.py
+++ Interface.py
@@ -336,106 +336,106 @@ class MouseReader:
         while not self.thread_end.is_set():
             # print('Reading file')
             data = self.file.read(3)  # Reads the 3 bytes
-            x, y = struct.unpack("2b", data[1:])
-            queue.put({'x': x/dpm, 'y': y/dpm, 'timestamp': time.time()})
+            self.x, self.y = struct.unpack("2b", data[1:])
+            self.queue.put({'x': x/dpm, 'y': y/dpm, 'timestamp': time.time()})
 
     def close(self):
         self.thread_end.set()
         self.thread_runner.join()
 
-class Writer(object):
-    """
-    Simple class to append value to a hdf5 file on disc (usefull for building k$
-
-    Params:
-        datapath: filepath of h5 file
-        dataset: dataset name within the file
-        shape: dataset shape (not counting main/batch axis)
-        dtype: numpy dtype
-
-    Usage:
-        hdf5_store = HDF5Store('/tmp/hdf5_store.h5','X', shape=(20,20,3))
-        x = numpy.random.random(hdf5_store.shape)
-        hdf5_store.append(x)
-        hdf5_store.append(x)
-    """
-
-    def __init__(self, datapath):
-        self.datapath = datapath
-        self.queue = Queue()
-        self.datasets = dict()
-        self.thread_end = threading.Event()
-        self.thread_runner = threading.Thread(target=self.dequeue)  # max insertion rate of 10 events/sec
-        self.thread_runner.start()
-
-    def createDataset(self, dataset, shape, dtype=np.int16, compression="gzip", chunk_len=1):
-        self.datasets[dataset] = self.h5Dataset(self.datapath, dataset, shape, dtype, compression, chunk_len)
-
-    def append(self, dataset, data):
-        self.queue.put({'dataset':dataset, 'data':data})
-
-    def dequeue(self):
-        while not self.thread_end.is_set():
-            if not self.queue.empty():
-                values = self.queue.get()
-                #if values['dataset'] == 'frames':
-                #    data = values['data']
-                with h5py.File(self.datapath, mode='a') as h5f:
-                    dset = h5f[values['dataset']]
-                    dset.resize((self.datasets[values['dataset']].i + 1, ) + self.dataset[values['dataset']].shape)
-                    dset[self.datasets[values['dataset']].i] = [values['data']]
-                    self.datasets[values['dataset']].i += 1
-                    h5f.flush()
-            else:
-                time.sleep(.1)
-
-    def exit(self):
-        while not self.queue.empty():
-            time.sleep(.1)
-        self.thread_end.set()
-
-    class h5Dataset():
-        def __init__(self, datapath, dataset, shape, dtype=np.uint16, compression="gzip", chunk_len=1):
-            with h5py.File(datapath, mode='a') as h5f:
-                self.i = 0
-                self.shape = shape
-                self.dtype = dtype
-                h5f.create_dataset(
-                    dataset,
-                    shape=(0,) + shape,
-                    maxshape=(None,) + shape,
-                    dtype=dtype,
-                    compression=compression,
-                    chunks=(chunk_len,) + shape)
-
-def handler(signal_received, frame):
-      # Handle any cleanup here
-      saver.exit()
-      print('SIGINT or CTRL-C detected. Exiting gracefully')
-      filename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".h5"
-      copyfile(filename, "/mnt/lab/users/sofia/tracking_" + filename)
-      print('Copying done, you can exit now')
-      mouse1.close();
-      mouse2.close();
-      exit(0)
-
-signal(SIGINT, handler)
-saver = Writer(filename)
-saver.createDataset('tracking_data', shape=(6,), dtype=TIME_SERIES_DOUBLE)
-
-while True:
-    # if keyboard.is_pressed('q'):  # if key 'q' is pressed
-    #        print('You Pressed A Key!')
-    #        copyfile(filename,"/mnt/lab/users/sofia/tracking_"+filename)
-    #        break
-    if getMouseEvent():
-       saver.append('tracking_data', [loc_x, loc_y, theta, timestamp])
-
-mouse1.close();
-mouse2.close();
-
-for channel in channels:
-    pwm[channel].stop()
-    GPIO.remove_event_detect(channel)
-    GPIO.cleanup()
-    print('cleaning done!')
+# class Writer(object):
+#     """
+#     Simple class to append value to a hdf5 file on disc (usefull for building k$
+#
+#     Params:
+#         datapath: filepath of h5 file
+#         dataset: dataset name within the file
+#         shape: dataset shape (not counting main/batch axis)
+#         dtype: numpy dtype
+#
+#     Usage:
+#         hdf5_store = HDF5Store('/tmp/hdf5_store.h5','X', shape=(20,20,3))
+#         x = numpy.random.random(hdf5_store.shape)
+#         hdf5_store.append(x)
+#         hdf5_store.append(x)
+#     """
+#
+#     def __init__(self, datapath):
+#         self.datapath = datapath
+#         self.queue = Queue()
+#         self.datasets = dict()
+#         self.thread_end = threading.Event()
+#         self.thread_runner = threading.Thread(target=self.dequeue)  # max insertion rate of 10 events/sec
+#         self.thread_runner.start()
+#
+#     def createDataset(self, dataset, shape, dtype=np.int16, compression="gzip", chunk_len=1):
+#         self.datasets[dataset] = self.h5Dataset(self.datapath, dataset, shape, dtype, compression, chunk_len)
+#
+#     def append(self, dataset, data):
+#         self.queue.put({'dataset':dataset, 'data':data})
+#
+#     def dequeue(self):
+#         while not self.thread_end.is_set():
+#             if not self.queue.empty():
+#                 values = self.queue.get()
+#                 #if values['dataset'] == 'frames':
+#                 #    data = values['data']
+#                 with h5py.File(self.datapath, mode='a') as h5f:
+#                     self.dset = h5f[values['dataset']]
+#                     self.dset.resize((self.datasets[values['dataset']].i + 1, ) + self.dataset[values['dataset']].shape)
+#                     self.dset[self.datasets[values['dataset']].i] = [values['data']]
+#                     self.datasets[values['dataset']].i += 1
+#                     h5f.flush()
+#             else:
+#                 time.sleep(.1)
+#
+#     def exit(self):
+#         while not self.queue.empty():
+#             time.sleep(.1)
+#         self.thread_end.set()
+#
+# class h5Dataset():
+#     def __init__(self, datapath, dataset, shape, dtype=np.uint16, compression="gzip", chunk_len=1):
+#         with h5py.File(datapath, mode='a') as h5f:
+#             self.i = 0
+#             self.shape = shape
+#             self.dtype = dtype
+#             h5f.create_dataset(
+#                 dataset,
+#                 shape=(0,) + shape,
+#                 maxshape=(None,) + shape,
+#                 dtype=dtype,
+#                 compression=compression,
+#                 chunks=(chunk_len,) + shape)
+#
+# def handler(signal_received, frame):
+#       # Handle any cleanup here
+#       saver.exit()
+#       print('SIGINT or CTRL-C detected. Exiting gracefully')
+#       filename = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".h5"
+#       copyfile(filename, "/mnt/lab/users/sofia/tracking_" + filename)
+#       print('Copying done, you can exit now')
+#       mouse1.close();
+#       mouse2.close();
+#       exit(0)
+#
+# signal(SIGINT, handler)
+# saver = Writer(filename)
+# saver.createDataset('tracking_data', shape=(6,), dtype=TIME_SERIES_DOUBLE)
+#
+# while True:
+#     # if keyboard.is_pressed('q'):  # if key 'q' is pressed
+#     #        print('You Pressed A Key!')
+#     #        copyfile(filename,"/mnt/lab/users/sofia/tracking_"+filename)
+#     #        break
+#     if getMouseEvent():
+#        saver.append('tracking_data', [loc_x, loc_y, theta, timestamp])
+#
+# self.mouse1.close();
+# self.mouse2.close();
+#
+# for channel in channels:
+#     pwm[channel].stop()
+#     GPIO.remove_event_detect(channel)
+#     GPIO.cleanup()
+#     print('cleaning done!')
