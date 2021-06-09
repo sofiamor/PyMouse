95ffc9e7b915794ed43a91894d9d9bd5f950cd27
sofiamor <med2p1080156@med.uoc.gr>
2021-06-09 16:46:39 +0300

    VR changes :)

--- utils/Writer.py
+++ utils/Writer.py
@@ -63,6 +63,8 @@ class Writer(object):
         self.thread_end.set()
         if self.target_path:
             copyfile(self.datapath, self.target_path + os.path.basename(datapath))
+            self.closeDatasets()
+            self.quit()
             print('file is copied')
 
     class h5Dataset():
