e37ad9cf45a206217ea2c8010524e4321fbb3ec6
sofiamor <med2p1080156@med.uoc.gr>
2021-06-09 15:46:20 +0300

    VR changes :)

--- utils/Writer.py
+++ utils/Writer.py
@@ -53,7 +53,7 @@ class Writer(object):
                     h5f.flush()
             else:
                 time.sleep(.1)
-            print('dequeueueueue')
+                print('dequeueueueue')
 
     def exit(self):
         while not self.queue.empty():
